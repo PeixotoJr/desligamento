45551aa2bb29db9f465eaefea9a7d1a53a1feef3
Julio Peixoto <juliojrps@gmail.com>
2024-12-19 17:18:21 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.title("Análise de Séries Temporais")
 st.sidebar.header("Configurações")
 
 # Upload do arquivo
-dados = st.file_uploader("Faça o upload do arquivo CSV", type=["csv"],accept_multiple_files=True)
+dados = st.file_uploader("Faça o upload do arquivo CSV", type=["csv","txt"])
 
 if dados is not None:
     # Parâmetros de configuração do upload
